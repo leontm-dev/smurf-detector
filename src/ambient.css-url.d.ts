dbcea220e1d56c882bf29d3cce05e5c6ce36ae61
Leon <LAnneken09@gmail.com>
2026-03-23 23:44:34 +0100

    Declared some types

---
+++ src/ambient.css-url.d.ts
@@ -0,0 +1,16 @@
+declare module '*.css?url' {
+  const href: string
+  export default href
+}
+declare module '*.scss?url' {
+  const href: string
+  export default href
+}
+declare module '*.sass?url' {
+  const href: string
+  export default href
+}
+declare module '*.less?url' {
+  const href: string
+  export default href
+}
